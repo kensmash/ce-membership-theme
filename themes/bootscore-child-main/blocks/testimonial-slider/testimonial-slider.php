c9b2bf0d9a87532f2a3bed8b982d7c110cf4caaa
Ken <kensmash@gmail.com>
2023-11-15 16:04:20 -0500

    fix content output from testimonials in slider block

--- themes/bootscore-child-main/blocks/testimonial-slider/testimonial-slider.php
+++ themes/bootscore-child-main/blocks/testimonial-slider/testimonial-slider.php
@@ -6,24 +6,8 @@
  */
 
 // Load values and assign defaults.
-$quote             = !empty(get_field( 'quote' )) ? get_field( 'quote' ) : 'Your quote here...';
-$author            = get_field( 'author' );
-$author_role       = get_field( 'role' );
-$image             = get_field( 'image' );
-$background_color  = get_field( 'background_color' ); // ACF's color picker.
-$text_color        = get_field( 'text_color' ); // ACF's color picker.
-$quote_attribution = '';
 
-if ( $author ) {
-    $quote_attribution .= '<footer class="testimonial__attribution">';
-    $quote_attribution .= '<cite class="testimonial__author">' . $author . '</cite>';
-
-    if ( $author_role ) {
-        $quote_attribution .= '<span class="testimonial__role">' . $author_role . '</span>';
-    }
-
-    $quote_attribution .= '</footer><!-- .testimonial__attribution -->';
-}
+$featured_testimonials = get_field('testimonials');
 
 // Support custom "anchor" values.
 $anchor = '';
@@ -32,39 +16,30 @@ if ( ! empty( $block['anchor'] ) ) {
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
-$class_name = 'testimonial';
+$class_name = 'testimonial-slider';
 if ( ! empty( $block['className'] ) ) {
     $class_name .= ' ' . $block['className'];
 }
 if ( ! empty( $block['align'] ) ) {
     $class_name .= ' align' . $block['align'];
 }
-if ( $background_color || $text_color ) {
-    $class_name .= ' has-custom-acf-color';
-}
-
-// Build a valid style attribute for background and text colors.
-$styles = array( 'background-color: ' . $background_color, 'color: ' . $text_color );
-$style  = implode( '; ', $styles );
 ?>
 
-<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>" style="<?php echo esc_attr( $style ); ?>">
-    <p>BLAHHHH</p>
-    <div class="testimonial__col">
-        <blockquote class="testimonial__blockquote">
-            <?php echo esc_html( $quote ); ?>
-
-            <?php if ( !empty( $quote_attribution ) ) : ?>
-                <?php echo wp_kses_post( $quote_attribution ); ?>
-            <?php endif; ?>
-        </blockquote>
-    </div>
-
-    <?php if ( $image ) : ?>
-        <div class="testimonial__col">
-            <figure class="testimonial__image">
-                <?php echo wp_get_attachment_image( $image['ID'], 'full', '', array( 'class' => 'testimonial__img' ) ); ?>
-            </figure>
+<div <?php echo esc_attr( $anchor ); ?>class="<?php echo esc_attr( $class_name ); ?>" style="">
+    <?php if( $featured_testimonials ): ?>
+        <div>
+        <?php foreach( $featured_testimonials as $testimonial ): 
+            $permalink = get_permalink( $testimonial->ID );
+            $title = get_the_title( $testimonial->ID );
+            $content = get_post_field('post_content', $testimonial->ID); 
+            $credentials = get_field( 'credentials', $testimonial->ID );
+            ?>
+            <div>
+                <?php echo esc_html( $title ); ?>
+                <span><?php echo esc_html( $credentials ); ?></span>
+                <?php echo $content; ?>
+            </div>
+        <?php endforeach; ?>
         </div>
     <?php endif; ?>
 </div>
