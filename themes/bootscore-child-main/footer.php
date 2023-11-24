05ab2262b0b560385312407fd5063617fee03408
Ken <kensmash@gmail.com>
2023-11-24 11:26:38 -0500

    footer stuff

--- themes/bootscore-child-main/footer.php
+++ themes/bootscore-child-main/footer.php
@@ -16,70 +16,78 @@
 
 <footer class="container-fluid ce-footer border-top">
 
-  <div class="container bootscore-footer pt-5 pb-3">
+  <div class="bootscore-footer pt-5 pb-3">
+    
+    <div class="container">
 
-      <div class="row">
+        <div class="row">
 
-        <div class="col">
-            <div>
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/logo/ce-logo-footer.svg" alt="Comics Experience logo">
-                </a>
-            </div>
-            <div>
-                <small class="bootscore-copyright"><span class="cr-symbol">&copy;</span>&nbsp;<?= date('Y'); ?> <?php bloginfo('name'); ?></small>
-            </div>
-            <div>
-                <?php if (is_active_sidebar('footer-social-media')) : ?>
-                    <?php dynamic_sidebar('footer-social-media'); ?>
-                <?php endif; ?>
-            </div>
-        </div><!-- .col -->
-        
-        <div class="col">
+            <div class="col-lg-3">
+                <div class="py-4 pe-4">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/logo/ce-logo-footer.svg" alt="Comics Experience logo">
+                    </a>
+                </div>
+                <div>
+                    <small class="bootscore-copyright"><span class="cr-symbol">&copy;</span>&nbsp;<?= date('Y'); ?> <?php bloginfo('name'); ?></small>
+                </div>
+                <div>
+                    <?php if (is_active_sidebar('footer-social-media')) : ?>
+                        <?php dynamic_sidebar('footer-social-media'); ?>
+                    <?php endif; ?>
+                </div>
+            </div><!-- .col -->
             
-        </div><!-- .col -->
+            <div class="col">
+                
+            </div><!-- .col -->
 
-      </div><!-- .row -->
+        </div><!-- .row -->
+
+    </div><!-- .container -->
+
+    <div class="container mt-4">
 
       <div class="row justify-content-space-between">
 
-        <!-- Footer 1 Widget -->
-        <div class="col-md-6 col-lg">
-          <?php if (is_active_sidebar('footer-1')) : ?>
-            <?php dynamic_sidebar('footer-1'); ?>
-          <?php endif; ?>
-        </div>
+            <!-- Footer 1 Widget -->
+            <div class="col-md-6 col-lg">
+            <?php if (is_active_sidebar('footer-1')) : ?>
+                <?php dynamic_sidebar('footer-1'); ?>
+            <?php endif; ?>
+            </div>
 
-        <!-- Footer 2 Widget -->
-        <div class="col-md-6 col-lg">
-          <?php if (is_active_sidebar('footer-2')) : ?>
-            <?php dynamic_sidebar('footer-2'); ?>
-          <?php endif; ?>
-        </div>
+            <!-- Footer 2 Widget -->
+            <div class="col-md-6 col-lg">
+            <?php if (is_active_sidebar('footer-2')) : ?>
+                <?php dynamic_sidebar('footer-2'); ?>
+            <?php endif; ?>
+            </div>
 
-        <!-- Footer 3 Widget -->
-        <div class="col-md-6 col-lg">
-          <?php if (is_active_sidebar('footer-3')) : ?>
-            <?php dynamic_sidebar('footer-3'); ?>
-          <?php endif; ?>
-        </div>
+            <!-- Footer 3 Widget -->
+            <div class="col-md-6 col-lg">
+            <?php if (is_active_sidebar('footer-3')) : ?>
+                <?php dynamic_sidebar('footer-3'); ?>
+            <?php endif; ?>
+            </div>
 
-        <!-- Footer 4 Widget -->
-        <div class="col-md-6 col-lg">
-          <?php if (is_active_sidebar('footer-4')) : ?>
-            <?php dynamic_sidebar('footer-4'); ?>
-          <?php endif; ?>
-        </div>
+            <!-- Footer 4 Widget -->
+            <div class="col-md-6 col-lg">
+            <?php if (is_active_sidebar('footer-4')) : ?>
+                <?php dynamic_sidebar('footer-4'); ?>
+            <?php endif; ?>
+            </div>
 
-        <!-- Footer 5 Widget -->
-        <div class="col-md-6 col-lg">
-          <?php if (is_active_sidebar('footer-5')) : ?>
-            <?php dynamic_sidebar('footer-5'); ?>
-          <?php endif; ?>
-        </div>
+            <!-- Footer 5 Widget -->
+            <div class="col-md-6 col-lg">
+            <?php if (is_active_sidebar('footer-5')) : ?>
+                <?php dynamic_sidebar('footer-5'); ?>
+            <?php endif; ?>
+            </div>
 
-      </div><!-- .row -->
+        </div><!-- .row -->
+
+      </div><!-- .container -->
 
     </div><!--  .container -->
 
