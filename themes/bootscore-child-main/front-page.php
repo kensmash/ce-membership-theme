c33d8f99d77b7c9cdeed21c997be7c891114b0ca
Ken <kensmash@gmail.com>
2023-12-09 08:40:50 -0500

    making subpages look better

--- themes/bootscore-child-main/front-page.php
+++ themes/bootscore-child-main/front-page.php
@@ -11,7 +11,7 @@
 get_header();
 ?>
 
-  <div id="content" class="site-content container-fluid">
+  <div id="content" class="site-content container-fluid bg-white">
     <div id="primary" class="content-area">
 
       <!-- Hook to add something nice -->
--- themes/bootscore-child-main/page.php
+++ themes/bootscore-child-main/page.php
@@ -19,7 +19,7 @@ get_header();
 <?php $avoid_fluid_container_post_types = array('forum', 'topic', 'reply'); ?>
 
   <div id="content" class="site-content container-fluid py-5 mt-5">
-    <div id="primary" class="content-area <?php echo is_cart() || in_array( get_post_type( get_the_id() ), $avoid_fluid_container_post_types ) ? "container" : "container"; ?>">
+    <div id="primary" class="content-area container bg-white rounded-1 px-4">
 
     <?php get_template_part( 'template-parts/content', 'topimage' ); ?>
 
