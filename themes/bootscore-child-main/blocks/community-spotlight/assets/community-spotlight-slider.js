cb6e64329fb815fe071a6f869abb8503da4993b0
Ken <kensmash@gmail.com>
2023-12-10 10:26:19 -0500

    fix centermode

--- themes/bootscore-child-main/blocks/community-spotlight/assets/community-spotlight-slider.js
+++ themes/bootscore-child-main/blocks/community-spotlight/assets/community-spotlight-slider.js
@@ -6,7 +6,7 @@
           mobileFirst: true,
           focusOnSelect: true,
           centerMode: true,
-          centerPadding: '60px',
+          centerPadding: '0',
           prevArrow:
             "<span class='slick-prev'><i class='fa-solid fa-chevron-left' aria-hidden='true'></i></span>",
           nextArrow:
