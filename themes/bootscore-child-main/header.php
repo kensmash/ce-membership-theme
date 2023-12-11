7221cc7a7a63ac5bda9617167135b3edb7a955b3
Ken <kensmash@gmail.com>
2023-12-11 11:38:58 -0500

    header margin logic

--- themes/bootscore-child-main/header.php
+++ themes/bootscore-child-main/header.php
@@ -33,34 +33,40 @@
 
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
 
-<div id="page" class="site">
+  <div id="page" class="site">
 
-  <header id="masthead" class="site-header">
+    <header id="masthead" class="site-header">
 
-    <?php $avoid_fixed_nav_post_types = array('courses', 'lesson', 'forum', 'topic', 'reply'); ?>
+      <?php 
+        $avoid_fixed_nav_post_types = array('courses', 'lesson', 'forum', 'topic', 'reply'); 
+        $header_margin_bottom = "";
+         if ( is_post_type_archive( 'courses' ) ) {
+            $header_margin_bottom = "mb-4";
+         }
+      ?>
 
-    <div class="<?php echo in_array( get_post_type( get_the_id() ), $avoid_fixed_nav_post_types ) ? "" : "fixed-top"; ?>">
+      <div class="<?php echo in_array( get_post_type( get_the_id() ), $avoid_fixed_nav_post_types ) ? $header_margin_bottom : "fixed-top"; ?>">
 
-      <nav id="nav-main" class="navbar navbar-expand-lg <?php echo is_front_page() ? "navbar-dark navbar-transparent navbar-dark" : "navbar-dark bg-dark"; ?>">
+        <nav id="nav-main" class="navbar navbar-expand-lg <?php echo is_front_page() ? "navbar-dark navbar-transparent navbar-dark" : "navbar-dark bg-dark"; ?>">
 
-        <div class="container-fluid">
+          <div class="container-fluid">
 
-          <!-- Navbar Brand -->
-          <a class="navbar-brand xs d-md-none" href="<?= esc_url(home_url()); ?>"><img src="<?= esc_url(get_stylesheet_directory_uri()); ?>/assets/images/logo/logo-sm.svg" alt="logo" class="logo xs"></a>
-          <a class="navbar-brand md d-none d-md-block" href="<?= esc_url(home_url()); ?>"><img src="<?= esc_url(get_stylesheet_directory_uri()); ?>/assets/images/logo/logo.svg" alt="logo" class="logo md"></a>
+            <!-- Navbar Brand -->
+            <a class="navbar-brand xs d-md-none" href="<?= esc_url(home_url()); ?>"><img src="<?= esc_url(get_stylesheet_directory_uri()); ?>/assets/images/logo/logo-sm.svg" alt="logo" class="logo xs"></a>
+            <a class="navbar-brand md d-none d-md-block" href="<?= esc_url(home_url()); ?>"><img src="<?= esc_url(get_stylesheet_directory_uri()); ?>/assets/images/logo/logo.svg" alt="logo" class="logo md"></a>
 
-          <!-- Offcanvas Navbar -->
-          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-navbar">
-            <div class="offcanvas-header">
-              <span class="h5 offcanvas-title"><?php esc_html_e('Menu', 'bootscore'); ?></span>
-              <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body justify-content-lg-center">
+            <!-- Offcanvas Navbar -->
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-navbar">
+              <div class="offcanvas-header">
+                <span class="h5 offcanvas-title"><?php esc_html_e('Menu', 'bootscore'); ?></span>
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div class="offcanvas-body justify-content-lg-center">
 
-              <!-- Bootstrap 5 Nav Walker Main Menu -->
-              <?php
+                <!-- Bootstrap 5 Nav Walker Main Menu -->
+                <?php
               wp_nav_menu(array(
                 'theme_location' => 'main-menu',
                 'container'      => false,
@@ -72,7 +78,7 @@
               ));
               ?>
 
-            <?php 
+                <?php 
 
             if ( is_user_logged_in() ) {
               //there is a user, is user a member?
@@ -83,7 +89,7 @@
                 <div class="top-nav-widget-2 d-lg-flex align-items-lg-center mt-2 mt-lg-0 ms-lg-2">
                   <a class="btn btn-success ms-lg-3 mt-3 mt-lg-0" href="<?php echo site_url('membership-levels'); ?>" role="button">Community Signup</a>
                 </div>
-              <?php 
+                <?php 
               } else { 
                 //we have a logged in member, show them a custom membership menu
                 wp_nav_menu(array(
@@ -103,19 +109,19 @@
                   <a href="<?php echo site_url('login'); ?>">Login</a>
                   <a class="btn btn-success ms-lg-3 mt-3 mt-lg-0" href="<?php echo site_url('membership-levels'); ?>" role="button">Community Signup</a>
                 </div>
-              <?php } ?>
+                <?php } ?>
 
+              </div>
             </div>
-          </div>
 
-          <div class="header-actions d-flex align-items-center">
+            <div class="header-actions d-flex align-items-center">
 
-            <!-- Top Nav Widget -->
-            <?php if (is_active_sidebar('top-nav')) : ?>
+              <!-- Top Nav Widget -->
+              <?php if (is_active_sidebar('top-nav')) : ?>
               <?php dynamic_sidebar('top-nav'); ?>
-            <?php endif; ?>
+              <?php endif; ?>
 
-            <?php
+              <?php
             if (class_exists('WooCommerce')) :
               get_template_part('template-parts/header/actions', 'woocommerce');
             else :
@@ -123,18 +129,18 @@
             endif;
             ?>
 
-            <!-- Navbar Toggler -->
-            <button class="btn btn-outline-light d-lg-none ms-1 ms-md-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-navbar" aria-controls="offcanvas-navbar">
-              <i class="fa-solid fa-bars"></i><span class="visually-hidden-focusable">Menu</span>
-            </button>
+              <!-- Navbar Toggler -->
+              <button class="btn btn-outline-light d-lg-none ms-1 ms-md-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-navbar" aria-controls="offcanvas-navbar">
+                <i class="fa-solid fa-bars"></i><span class="visually-hidden-focusable">Menu</span>
+              </button>
 
-          </div><!-- .header-actions -->
+            </div><!-- .header-actions -->
 
-        </div><!-- bootscore_container_class(); -->
+          </div><!-- bootscore_container_class(); -->
 
-      </nav><!-- .navbar -->
+        </nav><!-- .navbar -->
 
-      <?php
+        <?php
       if (class_exists('WooCommerce')) :
         get_template_part('template-parts/header/top-nav-search-collapse', 'woocommerce');
       else :
@@ -142,13 +148,13 @@
       endif;
       ?>
 
-    </div><!-- .fixed-top .bg-light -->
+      </div><!-- .fixed-top .bg-light -->
 
-    <!-- Offcanvas User and Cart -->
-    <?php
+      <!-- Offcanvas User and Cart -->
+      <?php
     if (class_exists('WooCommerce')) :
       get_template_part('template-parts/header/offcanvas', 'woocommerce');
     endif;
     ?>
 
-  </header><!-- #masthead -->
+    </header><!-- #masthead -->
