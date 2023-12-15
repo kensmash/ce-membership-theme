029511f71966ca26876b6a62d56c92f6ac5d34bc
Ken <kensmash@gmail.com>
2023-12-15 14:09:43 -0500

    mobile style updates

--- themes/bootscore-child-main/theme_components/js/home.js
+++ themes/bootscore-child-main/theme_components/js/home.js
@@ -6,10 +6,12 @@ jQuery(function($) {
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         //console.log("scroll? ", scroll)
-        if (scroll >= 400) {
-            header.removeClass('navbar-transparent').addClass('bg-dark');
-        } else {
-            header.removeClass('bg-dark').addClass('navbar-transparent');
+        if ($(window).width() >= 1024) {
+            if (scroll >= 400) {
+                header.removeClass('navbar-transparent').addClass('bg-dark');
+            } else {
+                header.removeClass('bg-dark').addClass('navbar-transparent');
+            }
         }
     });
   });
