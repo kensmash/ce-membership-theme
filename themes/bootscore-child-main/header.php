f4c254a01e2fb6276572a70f14e428e60f2a4b1e
Ken <kensmash@gmail.com>
2023-12-27 17:18:10 -0500

    conditional header menu depending on membership level

--- themes/bootscore-child-main/header.php
+++ themes/bootscore-child-main/header.php
@@ -92,23 +92,23 @@
               //there is a user, is user a member?
               //if not a member, show signup button
               $pmp_member = pmpro_getMembershipLevelForUser(get_current_user_id());
-              //echo var_dump($pmp_member);
+              //echo "member level: " . var_dump($pmp_member);
               if( !$pmp_member ) { ?>
                 <div class="top-nav-widget-2 d-lg-flex align-items-lg-center mt-2 mt-lg-0 ms-lg-2">
                   <a class="btn btn-success ms-lg-3 mt-3 mt-lg-0" href="<?php echo site_url('community'); ?>" role="button">Community Signup</a>
                 </div>
                 <?php 
               } else { 
-                //we have a logged in member, show them a custom membership menu
-                wp_nav_menu(array(
-                  'menu' => 'Community Links',
-                  'container'      => false,
-                  'menu_class'     => '',
-                  'fallback_cb'    => '__return_false',
-                  'items_wrap'     => '<ul id="bootscore-navbar" class="navbar-nav ms-auto ms-lg-0 %2$s">%3$s</ul>',
-                  'depth'          => 2,
-                  'walker'         => new bootstrap_5_wp_nav_menu_walker()
-                ));
+                 //we have a logged in member, show them a custom membership menu
+                  wp_nav_menu(array(
+                    'menu'           => $pmp_member->name == 'Tier 2' ? 'Tier 2 Community Links' : 'Tier 1 Community Links',
+                    'container'      => false,
+                    'menu_class'     => '',
+                    'fallback_cb'    => '__return_false',
+                    'items_wrap'     => '<ul id="bootscore-navbar" class="navbar-nav ms-auto ms-lg-0 %2$s">%3$s</ul>',
+                    'depth'          => 2,
+                    'walker'         => new bootstrap_5_wp_nav_menu_walker()
+                  ));
                }
             } else { 
               //no logged in user, show signup and login buttons
