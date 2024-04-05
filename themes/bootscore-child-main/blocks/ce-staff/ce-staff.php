1b16c27596112984db5c3e676206e4f87bb5573d
Ken <kensmash@gmail.com>
2024-04-05 09:58:31 -0400

    mo staff block styling

--- themes/bootscore-child-main/blocks/ce-staff/ce-staff.php
+++ themes/bootscore-child-main/blocks/ce-staff/ce-staff.php
@@ -26,37 +26,33 @@ if ( ! empty( $block['align'] ) ) {
 
 <div <?php echo esc_attr( $anchor ); ?> class="<?php echo esc_attr( $class_name ); ?>" style="">
 
-    <div class="row">
+    <div class="row justify-content-center">
 
         <?php 
             if( $staff_members ):
             foreach( $staff_members as $staff ): 
                 $permalink = get_permalink( $staff->ID );
                 $title = get_the_title( $staff->ID );
-                $image = get_the_post_thumbnail( $staff->ID, 'thumbnail', array( 'class' => 'rounded-left' ) );  
+                $image = get_the_post_thumbnail( $staff->ID, 'full', array( 'class' => 'rounded-top' ) );  
                 //$content = get_post_field('post_content', $service->ID); 
                 $content = get_the_excerpt($staff->ID); 
             ?>
 
-            <div class="col-md-6 col-xl-4">
+            <div class="col-sm-6 col-md-3 mb-3">
                 <div class="card h-100">
                     
-                    <div class="row g-0">
-                        <div class="col-4">
-                            <?php if ( $image ) { 
-                                    echo $image;
-                                } else { ?>
-                                    <img src="<?php echo esc_url( get_stylesheet_directory_uri() ) ?>/assets/images/ce-bio-placeholder.png" class="rounded-left" alt="Generic Staff Thumbnail">
-                            <?php } ?>
-                        </div> <!-- col -->
-                        <div class="col-8">
-                            <div class="card-body">
-                                <h5 class="card-title mb-2"><a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a></h5>
-                                <p class="card-text staff-meta text-muted"><?php echo $content; ?></p>
-                            </div>
-                        </div> <!-- col -->
-                    </div> <!-- row -->
-
+                    <?php if ( $image ) { 
+                            echo $image;
+                        } else { ?>
+                            <img src="<?php echo esc_url( get_stylesheet_directory_uri() ) ?>/assets/images/ce-bio-placeholder.png" class="rounded-top" alt="Generic Staff Thumbnail">
+                    <?php } ?>
+            
+                
+                    <div class="card-body">
+                        <h5 class="card-title mb-2"><a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a></h5>
+                        <p class="card-text staff-meta text-muted"><?php echo $content; ?></p>
+                    </div>
+                        
                 </div><!-- card -->
             </div> <!-- col -->
 
