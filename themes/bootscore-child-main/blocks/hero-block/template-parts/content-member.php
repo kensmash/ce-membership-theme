d0b1a1522abe7433e06fcbf60615e6337d2ff0e8
Ken <kensmash@gmail.com>
2024-04-22 16:45:17 -0400

    courses links

--- themes/bootscore-child-main/blocks/hero-block/template-parts/content-member.php
+++ themes/bootscore-child-main/blocks/hero-block/template-parts/content-member.php
@@ -1,11 +1,11 @@
 <?php global $current_user;
 wp_get_current_user(); ?>
 
-<h1>Welcome, <?php echo $current_user->display_name; ?></h1>
+<h1>Welcome, <?php echo $current_user->user_firstname; ?></h1>
 
 <p>Text here</p>
 
 <div class="d-grid gap-2">
   <button class="btn btn-primary" type="button">Button</button>
-  <button class="btn btn-primary" type="button">Button</button>
+  <a class="btn btn-primary" href="<?php echo site_url('my-account/my-courses'); ?>">Go to Your Courses</a>
 </div>
--- themes/bootscore-child-main/blocks/hero-block/template-parts/content-user.php
+++ themes/bootscore-child-main/blocks/hero-block/template-parts/content-user.php
@@ -1,13 +1,13 @@
 <?php global $current_user;
 wp_get_current_user(); ?>
 
-<h1>Welcome, <?php echo $current_user->display_name; ?></h1>
+<h1>Welcome, <?php echo $current_user->user_firstname; ?></h1>
 
 <p>Text here</p>
 
 <div class="d-grid gap-2">
   <?php if (learndash_user_get_enrolled_courses(get_current_user_id())): ?>
-    <a class="btn btn-primary" href="<?php echo site_url('my-account/my-courses'); ?>">Go to Courses</a>
+    <a class="btn btn-primary" href="<?php echo site_url('my-account/my-courses'); ?>">Go to Your Courses</a>
   <?php endif; ?>
     <a class="btn btn-success" href="<?php echo site_url('community'); ?>">Get Free Trial</a>
 </div>
