f56f8bb6fbef5758365a3dcc4025879b3752ef1c
Ken <kensmash@gmail.com>
2024-04-29 09:01:35 -0400

    add ACF user message to hero block

--- themes/bootscore-child-main/blocks/hero-block/template-parts/content-user.php
+++ themes/bootscore-child-main/blocks/hero-block/template-parts/content-user.php
@@ -3,7 +3,7 @@ wp_get_current_user(); ?>
 
 <h1>Welcome, <?php echo $current_user->user_firstname; ?>!</h1>
 
-<p>Member messsage here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<?php echo wp_kses_post ( get_field('user_message') ); ?>
 
 <div class="d-grid gap-2 pt-3">
   <?php if (learndash_user_get_enrolled_courses(get_current_user_id())): ?>
