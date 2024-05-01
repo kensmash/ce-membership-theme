82b29a4c5c9904443e58819fbafa699e3f6b1ffc
Ken <kensmash@gmail.com>
2024-05-01 12:58:18 -0400

    mo responsive hero block styles

--- themes/bootscore-child-main/blocks/hero-block/assets/hero-block.asset.php
+++ themes/bootscore-child-main/blocks/hero-block/assets/hero-block.asset.php
@@ -6,5 +6,5 @@
         'wp-element',
         'wp-i18n',
     ),
-    'version' => '4.3',
+    'version' => '4.5',
   );
--- themes/bootscore-child-main/blocks/hero-block/hero-block.php
+++ themes/bootscore-child-main/blocks/hero-block/hero-block.php
@@ -31,19 +31,19 @@ if ( $background_color || $text_color ) {
 }
 
 // Build a valid style attribute for background and text colors.
-//$styles = array( 'background-color: ' . $background_color, 'background-image: ' . $background_image, 'color: ' . $text_color );
-$styles = array( 'background-color: ' . $background_color, wp_is_mobile() ? '' : 'background-image: ' . $background_image, '; color: ' . $text_color );
+$styles = array( 'background-color: ' . $background_color, 'background-image: ' . $background_image, 'color: ' . $text_color );
+//$styles = array( 'background-color: ' . $background_color, wp_is_mobile() ? '' : 'background-image: ' . $background_image, '; color: ' . $text_color );
 $style  = implode( '; ', $styles );
 ?>
 
-<div <?php echo esc_attr( $anchor ); ?>class="container-fluid <?php echo esc_attr( $class_name );?> pt-4 pt-lg-5" style="<?php echo esc_attr( $style ); ?>">
+<div <?php echo esc_attr( $anchor ); ?>class="container-fluid <?php echo esc_attr( $class_name );?> px-0 pt-4 pt-lg-5" style="<?php echo esc_attr( $style ); ?>">
     <div class="container hero-block-container">
         <div class="row justify-content-lg-between row-cols-1 row-cols-sm-1 row-cols-md-2">
            
             <div class="col">
             </div>
 
-            <div class="col col-xl-4 col-xxl-5">
+            <div class="col hero-block-content-container col-xl-4 col-xxl-5">
 
             <?php if ( is_user_logged_in() ) {
               //there is a user, is user a member?
