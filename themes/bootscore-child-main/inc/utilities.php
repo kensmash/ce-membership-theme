bada18f68af5f82ed3b2289e021fcb4da12722fc
Ken <kensmash@gmail.com>
2024-05-01 15:28:15 -0400

    change defaut email from name and address

--- themes/bootscore-child-main/inc/utilities.php
+++ themes/bootscore-child-main/inc/utilities.php
@@ -10,3 +10,17 @@ function include_ipad_in_mobile_view( $is_mobile ) {
     return $is_mobile;
 }
 add_filter( 'wp_is_mobile', 'include_ipad_in_mobile_view' );
+
+/* change default email from name in WordPress */
+/* https://www.wpbeginner.com/plugins/how-to-change-sender-name-in-outgoing-wordpress-email/ */
+
+// Please edit the address and name below.
+// Change the From address.
+add_filter( 'wp_mail_from', function ( $original_email_address ) {
+    return 'info.comicsexperience.com';
+} );
+ 
+// Change the From name.
+add_filter( 'wp_mail_from_name', function ( $original_email_from ) {
+    return 'Comics Experience';
+} );
