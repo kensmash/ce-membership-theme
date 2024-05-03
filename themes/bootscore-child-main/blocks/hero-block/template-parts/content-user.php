955e944afa98b5fa4a6ebdd1a482c4c241615602
Ken <kensmash@gmail.com>
2024-05-03 16:06:09 -0400

    fix ACF warning

--- themes/bootscore-child-main/blocks/hero-block/template-parts/content-user.php
+++ themes/bootscore-child-main/blocks/hero-block/template-parts/content-user.php
@@ -9,5 +9,5 @@ wp_get_current_user(); ?>
   <?php if (learndash_user_get_enrolled_courses(get_current_user_id())): ?>
     <a class="btn btn-primary" href="<?php echo site_url('my-courses'); ?>">Go to Your Courses</a>
   <?php endif; ?>
-    <a class="btn btn-success" href="<?php echo site_url('community'); ?>">Get Free Trial</a>
+    <a class="btn btn-success" href="<?php echo site_url('community'); ?>">Free Trial Membership</a>
 </div>
