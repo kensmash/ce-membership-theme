ab2a0ad7879799f5ef502102143e9e37ca84f461
Ken <kensmash@gmail.com>
2024-05-10 10:49:55 -0400

    add archive pages

---
+++ themes/bootscore-child/archive-podcast.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * The template for displaying the podcast archive page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+
+<div id="content" class="site-content <?= apply_filters('bootscore/class/container', 'container-fluid', 'page'); ?> <?= apply_filters('bootscore/class/content/spacer', 'px-0 py-lg-5 mt-lg-1', 'page'); ?>">
+    <div id="primary" class="content-area <?= apply_filters('bootscore/class/container', 'container bg-white', 'page'); ?> <?= apply_filters('bootscore/class/content/spacer', 'px-0', 'page'); ?>">
+    
+    <?php if (!wp_is_mobile()): get_template_part( 'template-parts/content', 'topimage' ); endif; ?>
+
+      <div class="row">
+
+        <div class="<?= apply_filters('bootscore/class/main/col', 'col px-4 px-xl-5 py-4'); ?>">
+
+          <main id="main" class="site-main">
+
+            <div class="page-header mb-4">
+              <?php the_archive_title('<h1>', '</h1>'); ?>
+              <?php the_archive_description('<div class="archive-description">', '</div>'); ?>
+            </div>
+
+            <?php if ( have_posts() ) : 
+        
+                $page = get_query_var('paged'); // get which page number we are on
+                $counter = 0;
+                $queried_object = get_queried_object();
+
+                set_query_var('podcast', true); 
+                $prevText = "Older episodes &raquo;";
+                $nextText = "&laquo; Newer episodes"; 
+		
+                /* Start the Loop */
+                while ( have_posts() ) :
+                    the_post();
+
+                    $counter++; // add +1 to count for each post
+                    /*
+                    * Include the Post-Type-specific template for the content.
+                    * If you want to override this in a child theme, then include a file
+                    * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                    */
+                    if ($counter === 1 AND $paged <= 1) {
+                        get_template_part( 'template-parts/content', get_post_type() );
+                    } else {
+                        get_template_part( 'template-parts/content-excerpt' );
+                    }
+
+                endwhile;
+			?>
+
+                <nav aria-label="Page navigation" class="col-12">
+                    <div class="row px-4">
+                        <div class="col-md-6 text-center text-md-start"><?php previous_posts_link( $nextText ); ?></div>
+                        <div class="col-md-6 text-center text-md-end"><?php next_posts_link( $prevText ); ?></div>
+                    </div>
+                </nav>
+
+	    <?php else :
+
+		    get_template_part( 'template-parts/content', 'none' );
+
+		    endif;
+
+		?>
+
+          </main>
+
+        </div>
+      
+      </div>
+
+    </div>
+  </div>
+
+<?php
+get_footer();
---
+++ themes/bootscore-child/archive-script.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * The template for displaying the script archive page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+
+<div id="content" class="site-content <?= apply_filters('bootscore/class/container', 'container-fluid', 'page'); ?> <?= apply_filters('bootscore/class/content/spacer', 'px-0 py-lg-5 mt-lg-1', 'page'); ?>">
+    <div id="primary" class="content-area <?= apply_filters('bootscore/class/container', 'container bg-white', 'page'); ?> <?= apply_filters('bootscore/class/content/spacer', 'px-0', 'page'); ?>">
+    
+    <?php if (!wp_is_mobile()): get_template_part( 'template-parts/content', 'topimage' ); endif; ?>
+
+      <div class="row">
+
+        <div class="<?= apply_filters('bootscore/class/main/col', 'col px-4 px-xl-5 py-4'); ?>">
+
+          <main id="main" class="site-main">
+
+            <div class="page-header mb-4">
+              <?php the_archive_title('<h1>', '</h1>'); ?>
+              <?php the_archive_description('<div class="archive-description">', '</div>'); ?>
+            </div>
+
+            <?php 
+
+                $args = array( 'post_type' => 'script','orderby'=> 'title', 'order' => 'ASC','posts_per_page' => -1 ); 
+
+                $the_query = new WP_Query( $args );
+                
+                if( $the_query->have_posts() ): ?>
+
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-controls="collapseOne">
+                                    The Comics Experience Script Template
+                                </button>
+                                </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body scripts">
+                                    <ul>
+                                        <li>
+                                            <a href="<?php bloginfo('template_directory'); ?>/media/Comic-Experience-Script-Template-2021-11-24.doc">
+                                                Suggested script format template from Comics Experience (Word)
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="heading<?php the_ID(); ?>">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php the_ID(); ?>" aria-expanded="false" aria-controls="collapse<?php the_ID(); ?>">
+                                    <?php the_title(); ?>
+                                </button>
+                            </h2>
+                            <div id="collapse<?php the_ID(); ?>" class="accordion-collapse collapse" aria-labelledby="heading<?php the_ID(); ?>" data-bs-parent="#accordionExample">
+                                <div class="accordion-body scripts">
+                                    <?php remove_filter ('the_content',  'wpautop'); ?>
+                                    <?php the_content();?>
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+
+                    <?php else:  ?>
+                        <p><?php  _e( 'Sorry, no scripts to display.' );  ?></p>
+                    <?php endif; ?>
+                    
+                </div><!-- accordion -->
+
+          </main>
+
+        </div>
+      
+      </div>
+
+    </div>
+  </div>
+
+<?php
+get_footer();
