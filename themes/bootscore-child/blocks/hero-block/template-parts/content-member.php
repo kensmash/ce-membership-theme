cc2af196555f9d06f2573db61387e3c647b1a6dd
Ken <kensmash@gmail.com>
2024-05-24 10:54:41 -0400

    hero block fixes

--- themes/bootscore-child/blocks/hero-block/template-parts/content-member.php
+++ themes/bootscore-child/blocks/hero-block/template-parts/content-member.php
@@ -4,19 +4,24 @@
   $pmp_member = pmpro_getMembershipLevelForUser(get_current_user_id());
 ?>
 
-<h1>Welcome, <?php echo $current_user->user_firstname; ?>!</h1>
+<h1>Welcome, <?php echo $current_user->display_name; ?>!</h1>
 
 <p>We&rsquo;re glad to have you back.</p>
 
-<div class="d-grid gap-2 pt-2">
-  <a class="btn btn-success" href="<?php echo site_url('my-courses'); ?>">Go to Your Courses</a>
-</div>
-
-
 <?php 
+  if ($pmp_member->name == 'Community' && learndash_user_get_enrolled_courses(get_current_user_id())): ?>
+    <a class="btn btn-primary" href="<?php echo site_url('my-courses'); ?>">Go to Your Courses</a>
+<?php 
+  endif; 
+
   if ($pmp_member->name == 'Community Pro'): ?>
+    <div class="d-grid gap-2 pt-2">
+      <a class="btn btn-success" href="<?php echo site_url('my-courses'); ?>">Go to Your Courses</a>
+    </div>
+
     <p class="mt-4 ms-2"><strong>Member Events</strong></p>
     <?php echo do_shortcode('[tribe_this_week]'); 
   endif; 
+
 ?>
 
