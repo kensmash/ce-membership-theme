8efcbec79c2fee9abd45d326113573cb6b3a30ff
Ken <kensmash@gmail.com>
2024-05-24 14:24:15 -0400

    update creative services block layout

--- themes/bootscore-child/blocks/creative-services/creative-services.php
+++ themes/bootscore-child/blocks/creative-services/creative-services.php
@@ -32,24 +32,35 @@
              if( $featured_services ):
              foreach( $featured_services as $service ): 
                  $permalink = get_permalink( $service->ID );
+                 $image = get_the_post_thumbnail( $staff->ID, 'full', array( 'class' => 'rounded-top' ) );  
                  $title = get_the_title( $service->ID );
                  //$content = get_post_field('post_content', $service->ID); 
                  $content = get_the_excerpt($service->ID); 
              ?>
  
-             <div class="col-md-6 col-xl-4 mb-3">
-                 <div class="card h-100">
-                     <h5 class="card-header py-4"><?php echo esc_html( $title ); ?></h5>
-                     <div class="card-body">
-                         <p class="card-text"><?php echo $content; ?></p>
+            <div class="col-sm-6 col-lg-4 mb-3 ">
+                <div class="card h-100">
+                    
+                    <a href="<?php echo esc_url( $permalink ); ?>">
+                        <?php if ( $image ) { 
+                                echo $image;
+                            } else { ?>
+                                <img src="<?php echo esc_url( get_stylesheet_directory_uri() ) ?>/assets/images/thumbnail-scifi.jpg" class="rounded-top" alt="Generic Staff Thumbnail">
+                        <?php } ?>
+                    </a>
+            
+                    <div class="card-body">
+                        <h5 class="card-title mb-2"><a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a></h5>
+                        <small class="card-text staff-meta text-muted"><?php echo $content; ?></small>
                      </div>
                      <div class="card-footer bg-transparent border-0 py-4">
                          <div class="d-grid">
-                             <a href="<?php echo esc_url( $permalink ); ?>" class="btn btn-primary px-md-5">Learn More</a>
+                             <a href="<?php echo esc_url( $permalink ); ?>" class="btn btn-success px-md-5">Learn More</a>
                          </div>
                      </div>
-                 </div><!-- card -->
-             </div> <!-- col -->
+                        
+                </div><!-- card -->
+            </div> <!-- col -->
  
          <?php 
              endforeach;
