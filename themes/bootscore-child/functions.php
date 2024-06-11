c071063f769e1509eebf86c8b027fa67bcf6adac
Ken <kensmash@gmail.com>
2024-06-11 11:55:28 -0400

    remove wc cart fragments function

--- themes/bootscore-child/functions.php
+++ themes/bootscore-child/functions.php
@@ -10,11 +10,6 @@
 // Exit if accessed directly
 defined('ABSPATH') || exit;
 
-function enqueue_wc_cart_fragments() { wp_enqueue_script( 'wc-cart-fragments' ); }
-add_action( 'wp_enqueue_scripts', 'enqueue_wc_cart_fragments' );
-
-
-
 /**
  * Enqueue scripts and styles
  */
