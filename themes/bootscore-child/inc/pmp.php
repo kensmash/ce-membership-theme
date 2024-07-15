beafb10c1467270a44d618efaab5796d15d306f0
Ken <kensmash@gmail.com>
2024-07-15 09:56:27 -0400

    update cc email address on member purchases

--- themes/bootscore-child/inc/pmp.php
+++ themes/bootscore-child/inc/pmp.php
@@ -169,3 +169,32 @@ function my_wp_bouncer_number_simultaneous_logins($num) {
 	return 3;
 }
 add_filter('wp_bouncer_number_simultaneous_logins', 'my_wp_bouncer_number_simultaneous_logins');
+
+
+/**
+ * BCC Additional Email Addresses on Member or Admin Notifications
+ * This will allow you to BCC additional email addresses for member and admin notifications.
+ *
+ * Ensure line 23 is changed to your preferred BCC email address
+ *
+ * title: BCC Additional Email Addresses on Member or Admin Notifications
+ * layout: snippet
+ * collection: email
+ * category: bcc
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+ // https://www.paidmembershipspro.com/bcc-additional-email-addresses-on-member-or-admin-notifications/
+ function my_pmpro_email_headers_admin_emails( $headers, $email ) {
+ 
+	// BCC emails already going to admin_email.
+	if ( strpos( $email->template, '_admin' ) !== false ) {
+		$headers[] = 'Bcc:' . 'info@comicsexperience.com';
+	}
+ 
+	return $headers;
+}
+add_filter( 'pmpro_email_headers', 'my_pmpro_email_headers_admin_emails', 10, 2 );
