04f6f4642b650b12f4766e737257602762fc8aae
Ken <kensmash@gmail.com>
2024-09-16 09:28:19 -0400

    set up courses slide on home page

--- themes/bootscore-child/blocks/ce-hero-slider/template-parts/slide-course.php
+++ themes/bootscore-child/blocks/ce-hero-slider/template-parts/slide-course.php
@@ -8,30 +8,46 @@
  */
 
 ?>
-<div class="container">
 
-    <div class="woocommerce card bg-dark text-white rounded-1 border-0">
+<?php
+$featured_courses = get_sub_field('course');
 
-    <?php 
-    $image = get_field('hero_image');
-    if( !empty( $image ) ) { ?>
-            
-            <img src="<?php echo esc_url($image['url']); ?>" class="card-img" alt="<?php echo esc_attr($image['alt']); ?>" />
-        
-        <?php } else { ?>
-        
-            <img src="<?php echo esc_url( get_template_directory_uri() ) ?>/images/gui/hero-bgs/hero-bg-fantasy.jpg" class="card-img" alt="Fantasy Illustration">
-            
-        <?php } ?>
+if( $featured_courses ): ?>
 
-        <div class="card-img-overlay">
-            <div class="col-md-8 col-lg-6 col-xl-5">
-                <h2 class="mt-2"><?php the_title(); ?></h2>
-                <div class="pr-4"><?php the_excerpt(); ?></div>
-            
-                <a href="<?php the_permalink();?>"><button type="button" class="btn btn-info mt-lg-3 border-0">View Course</button></a>
-            </div>
+    <div class="container">
+
+    <?php foreach( $featured_courses as $featured_course ): 
+
+        $permalink = get_permalink( $featured_course->ID );
+        $title = get_the_title( $featured_course->ID );
+        $excerpt = get_the_excerpt( $featured_course->ID );
+        $image = get_field( 'hero_image', $featured_course->ID ); ?>
+
+        <div>
+
+        <?php if( !empty( $image ) ) { ?>
+                    
+                    <img src="<?php echo esc_url($image['url']); ?>" class="card-img" alt="<?php echo esc_attr($image['alt']); ?>" />
+                
+                <?php } else { ?>
+                
+                    <img src="<?php echo esc_url( get_stylesheet_directory_uri() ) ?>/images/gui/hero-bgs/hero-bg-fantasy.jpg" class="card-img" alt="Fantasy Illustration">
+                    
+                <?php } ?>
+               
+                        <h2 class="mt-2"><?php echo $title; ?></h2>
+                        <div class="pr-4"><?php echo $excerpt; ?></div>
+                    
+                        <a href="<?php echo $permalink; ?>"><button type="button" class="btn btn-info mt-lg-3 border-0">View Course</button></a>
+
         </div>
-    </div>
 
-</div>
+    <?php endforeach; ?>
+
+    </div> <!-- container -->
+
+<?php endif; ?>
+
+
+
+
